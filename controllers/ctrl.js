20b3096b5de5b72883d444ceaa05a97db11ca3cc
KhoiLe <Khoilecontact@gmail.com>
2021-10-27 14:55:43 +0700

    working on sign up

--- controllers/ctrl.js
+++ controllers/ctrl.js
@@ -39,5 +39,22 @@ module.exports.postLoginUser = (req, res) => {
 
     req.session.user = user;
     res.redirect('/');
+}
 
+// Sign up user 
+module.exports.signUpUser = (req, res) => {
+    if(res.locals.user || res.locals.admin) {
+        delete res.locals.user;
+        delete res.locals.admin;
+    };
+
+    res.render('registration');
+}
+
+// Sign up user controller 
+module.exports.postSignUpUser = (req, res) => {
+    const Userdata = {
+        email: req.body.email,
+        password: req.body.password
+    };
 }
