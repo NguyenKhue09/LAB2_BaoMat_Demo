9053560cd0630056153d9a2454acf2b6c4a45a9b
huyvouit <huyvouit@gmail.com>
2021-10-27 15:49:50 +0700

    auth html

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const express = require('express');
-const app = express() 
+const express = require("express");
+const app = express();
 const port = 1050;
 
 // Libraries
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 // Routes
-const loginRoute = require('./routes/login.route');
+const loginRoute = require("./routes/login.route");
 
-require('dotenv').config(); // to use .env file 
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
-app.set('views', './views'); // view 
+require("dotenv").config(); // to use .env file
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
+app.set("views", "./views"); // view
+app.use(express.static("public"));
 app.use(cookieParser(process.env.SECRET_COOKIES));
-app.use(session({
+app.use(
+  session({
     secret: process.env.SECRET_SESSION,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: parseInt(process.env.SESSION_TIMEOUT) || 60000000 }
-}));
+    cookie: { maxAge: parseInt(process.env.SESSION_TIMEOUT) || 60000000 },
+  })
+);
 
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        user: res.locals.user
-    });
+app.get("/", (req, res) => {
+  res.render("authentication", {
+    user: res.locals.user,
+  });
 });
 
-app.use('/login', loginRoute);
-
+app.use("/login", loginRoute);
 
 app.listen(port, () => {
-    // check if the website is runnig
-    console.log(`The app is listening at port ${port}`);
+  // check if the website is runnig
+  console.log(`The app is listening at port ${port}`);
 });
