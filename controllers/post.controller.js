14c8ca074e1ff2bbd3987067ff5c18f86633a4b3
KhoiLe <Khoilecontact@gmail.com>
2021-11-03 10:10:02 +0700

    pending post function

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,6 +1,44 @@
 
+async function initPost (req, res) {
+    // Dữ liệu bài đang mới 
+    const newPost = {
+        title: req.body.title,
+        description: req.body.description,
+        userPostId: req.signedCookies.userId,
+    };
+
+
+}
+
+async function deletePost (req, res) {
+    const postId = req.body.id;
+
+    // Thực hiện xoá
+
+}
+
+async function updatePost (req, res) {
+    const newData = {
+        title: req.body.title,
+        description: req.body.description,
+    }
+
+    // Cập nhật dữ liệu của bài post
+}
+
+// Đăng 1 bình luận mới 
+async function addComment (req, res) {
+    const newComment = {
+        text: req.body.text,
+        post: req.body.postId,
+        userComment: req.signedCookies.userId,
+    }
+}
 
 
 module.exports = {
-
+    initPost,
+    deletePost,
+    updatePost,
+    addComment,
 };
