e084b139b234447f377516835f0361aeef665986
Khuê Nguyễn <19521712@ms.uit.edu.vn>
2021-11-03 10:24:34 +0700

    add more function

--- services/post.service.js
+++ services/post.service.js
@@ -41,10 +41,105 @@ async function getPostByUserId(userId) {
     return posts;
 }
 
+async function getPostByPage(page) {
+    try {
+        if(page < 1) throw "Page must be bigger one";
+
+        const postList = await Post.find().limit(10).skip(page*10).lean();
+
+        if(!postList) throw "Page list not found!"
+        return postList;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+async function getNumberOfPost() {
+    try {
+        const numberOfPost = await Post.estimatedDocumentCount();
+
+        if(!numberOfPost) throw "Number of post not found!";
+
+        return numberOfPost;
+    } catch (error) {
+        console.log(error)
+        return null;
+    }
+}
+
+async function deletePost(postId) {
+
+    const sess = await Post.startSession();
+    sess.startTransaction();
+
+    try {
+
+        const deletePost = await Post.findByIdAndDelete(postId, {session: sess});
+
+        if(deletePost) {
+            const deleteComment = await deleteCommentByPost(postId);
+
+            if(deleteComment) {
+                console.log("Delete comment belong to post successful");
+                await sess.commitTransaction();
+            } else {
+                throw "Delete comment belong to post fail!";
+            }
+        } else {
+            throw "Post not found!";
+        }
+
+        sess.endSession();
+        return true;
+        
+    } catch (error) {
+        console.log(error);
+        await sess.abortTransaction();
+        sess.endSession();
+        return false;
+    }
+}
+
+async function updatePost(postId, newData) {
+    try{
+
+        let data = {};
+        if(newData.title != null) {
+            data = {title: newData.title};
+        } 
+
+        if(newData.description != null) {
+            data = {...data, description: newData.description};
+        }
+
+        const update = await Post.findOneAndUpdate({
+            _id: postId,
+        },data)
+
+        if(!update) throw "Update post fail!";
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function addCommentToPost(postId, commentId) {
+
+}
+
 
 
 module.exports = {
     getPostByUserId,
-    getAllPost
+    getAllPost,
+    getPostByPage,
+    updatePost,
+    deletePost
 }
 
+
+
+const {deleteCommentByPost} = require('../services/comment.service');
