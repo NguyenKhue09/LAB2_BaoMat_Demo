9ec3c3524b926db99cac48315d38db72d7024fea
huyvouit <huyvouit@gmail.com>
2021-11-12 22:14:43 +0700

    add post comment

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,83 +1,82 @@
-const postService = require('../services/post.service');
-const commentService = require('../services/comment.service');
+const postService = require("../services/post.service");
+const commentService = require("../services/comment.service");
 
-async function initPost (req, res) {
-    // Dữ liệu bài đang mới 
-    const newPost = {
-        title: req.body.title,
-        description: req.body.description,
-        userPostId: req.signedCookies.userId,
-    };
+async function initPost(req, res) {
+  // Dữ liệu bài đang mới
+  const newPost = {
+    title: req.body.title,
+    description: req.body.description,
+    userPostId: req.signedCookies.userId,
+  };
 
-    const result = await postService.addPost(newPost);
+  const result = await postService.addPost(newPost);
 
-    if (result) {
-        console.log("Created post");
-    } else {
-        console.log("Falied in creating post");
-    }
+  if (result) {
+    console.log("Created post");
+  } else {
+    console.log("Falied in creating post");
+  }
 
-    res.redirect('/');
+  res.redirect("/");
 }
 
-async function deletePost (req, res) {
-    const postId = req.body.id;
+async function deletePost(req, res) {
+  const postId = req.body.id;
 
-    // Thực hiện xoá
-    const result = await postService.deletePost(postId);
+  // Thực hiện xoá
+  const result = await postService.deletePost(postId);
 
-    if (result) {
-        console.log("Deleted post");
-    } else {
-        console.log("Falied in deleting post");
-    }
+  if (result) {
+    console.log("Deleted post");
+  } else {
+    console.log("Falied in deleting post");
+  }
 
-    res.redirect('/');
+  res.redirect("/");
 }
 
-async function updatePost (req, res) {
-    const postId = req.body.id;
+async function updatePost(req, res) {
+  const postId = req.body.id;
 
-    const newData = {
-        title: req.body.title,
-        description: req.body.description,
-    }
+  const newData = {
+    title: req.body.title,
+    description: req.body.description,
+  };
 
-    // Cập nhật dữ liệu của bài post
-    const result = await postService.updatePost(postId, newData);
+  // Cập nhật dữ liệu của bài post
+  const result = await postService.updatePost(postId, newData);
 
-    if (result) {
-        console.log("Updated post");
-    } else {
-        console.log("Falied in updating post");
-    }
+  if (result) {
+    console.log("Updated post");
+  } else {
+    console.log("Falied in updating post");
+  }
 
-    res.redirect('/');
+  res.redirect("/");
 }
 
-// Đăng 1 bình luận mới 
-async function addComment (req, res) {
-    const newComment = {
-        text: req.body.text,
-        post: req.body.postId,
-        userComment: req.signedCookies.userId,
-    }
+// Đăng 1 bình luận mới
+async function addComment(req, res) {
+  const newComment = {
+    text: req.body.text,
+    post: req.body.postId,
+    userComment: req.signedCookies.userId,
+  };
 
-    const result = await commentService.addComment(newComment);
+  const result = await commentService.addComment(newComment);
 
-    if (result) {
-        console.log("Added comment to post");
-    } else {
-        console.log("Falied in adding comment to post");
-    }
+  if (result) {
+    console.log("Added comment to post");
+  } else {
+    console.log("Falied in adding comment to post");
+  }
 
-    res.redirect('/');
+  res.redirect("/");
 }
-
 
 module.exports = {
-    initPost,
-    deletePost,
-    updatePost,
-    addComment,
+  initPost,
+  deletePost,
+  updatePost,
+  addComment,
 };
